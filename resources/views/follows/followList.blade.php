6ea4a2861ed0764e623516f6af78f83115921066
DESKTOP-31QP292\gaga0 <niconicochan1012@gmail.com>
2025-01-03 09:56:17 +0900

    変更

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.login')
+
 {{-- @extends('ビューファイル名') --}}
 {{-- 記述した場所に、指定したファイルの中身をそのまま表示 --}}
 
 @section('content')
+    {{-- フォローしているユーザーのアイコンを表示 --}}
+    <div class="container">
+        <section class="Follow-List">
+            <h1>Follow List</h1>
+            <div class="Follow-items">
+                @foreach ($follows as $follow)
+                    {{-- シンボリックリンクから画像を取得 --}}
+                    <img src="{{ asset('storage/' . $follow->images) }}">
+                @endforeach
+            </div>
+            {{-- フォローしているユーザーの投稿を表示 --}}
+            @foreach ($posts as $post)
+                <p><img src="{{ asset('storage/' . $follow->images) }}"></p>
+                <p>:{{ $post->user->username }}</p>
+                <p>:{{ $post->post }}</p>
+                <p>:{{ $post->created_at }}</p>
+            @endforeach
+        </section>
+    </div>
 @endsection
--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -1,4 +1,28 @@
 @extends('layouts.login')
 
 @section('content')
+    {{-- フォローされているユーザーのアイコンを表示 --}}
+    <div class="container">
+        <section class="Follow-List">
+            <h1>Follower List</h1>
+            <div class="Follow-items">
+                @foreach ($followers as $follower)
+                    {{-- シンボリックリンクから画像を取得 --}}
+                    <img src="{{ asset('storage/' . $follower->images) }}">
+                @endforeach
+            </div>
+
+            {{-- フォローされているいるユーザーの投稿を表示 --}}
+            @foreach ($posts as $post)
+                <div class="Post-item">
+                    {{-- 投稿者のアイコン --}}
+                    <img src="{{ asset('storage/' . $follower->images) }}">
+                    {{-- 投稿内容 --}}
+                    <p>名前: {{ $post->user->username }}</p>
+                    <p>投稿内容: {{ $post->post }}</p>
+                    <p>投稿日: {{ $post->created_at }}</p>
+                </div>
+            @endforeach
+        </section>
+    </div>
 @endsection
